e62507fd91a6334e4d8493f289516692fb4fbb61
Aditya Maurya <21bce056@nith.ac.in>
2023-12-16 12:58:50 +0530

    solved day 5 part 2

--- day5.py
+++ day5.py
@@ -4,7 +4,7 @@
 # with open('input.txt') as f:
 #     lines = f.read().strip().split("\n\n")
 
-# seeds = list(map(int,lines.pop(0).split()[1:]))
+# seeds = list(map(int, lines.pop(0).split()[1:]))
 # maps = []
 # i = 0
 # while i < len(lines):
@@ -14,10 +14,10 @@
 #     j = 0
 #     while j < len(temp_map):
 #         maps[-1].append(list(map(int, temp_map[j].split())))
-#         j+=1
-#     i+=1
-    
-    
+#         j += 1
+#     i += 1
+
+
 # def findLocation(seed):
 #     curr = seed
 #     for m in maps:
@@ -27,21 +27,26 @@
 #                 break
 #     return curr
 
+
 # ans = 10000000000
 # for seed in seeds:
 #     location = findLocation(seed)
-#     if(ans > location):
+#     if (ans > location):
 #         ans = location
-    
+
 # print(ans)
-    
-    
+
+
 # Part 2
 
 with open('input.txt') as f:
     lines = f.read().strip().split("\n\n")
 
-seeds = list(map(int,lines.pop(0).split()[1:]))
+inputs = list(map(int, lines.pop(0).split()[1:]))
+seeds = []
+for i in range(0, len(inputs), 2):
+    seeds.append((inputs[i], inputs[i]+inputs[i+1]))
+
 maps = []
 i = 0
 while i < len(lines):
@@ -51,24 +56,25 @@ while i < len(lines):
     j = 0
     while j < len(temp_map):
         maps[-1].append(list(map(int, temp_map[j].split())))
-        j+=1
-    i+=1
-    
-    
-def findLocation(seed):
-    curr = seed
-    for m in maps:
+        j += 1
+    i += 1
+print(seeds)
+for m in maps:
+    locations = []
+    while seeds:
+        s, e = seeds.pop()
         for dest, src, range_len in m:
-            if src <= curr < src+range_len:
-                curr = dest + (curr - src)
+            os = max(s, src) # overlapping start 
+            oe = min(e, src+range_len) # overlapping end 
+            if( os < oe ): 
+                locations.append((dest + os - src, dest + oe - src))
+                if os > s:
+                    seeds.append((s, os))
+                if oe < e:
+                    seeds.append((oe, e))                
                 break
-    return curr
+        else:
+            locations.append((s,e))
+    seeds = locations
 
-ans = 10000000000
-for seed in seeds:
-    location = findLocation(seed)
-    if(ans > location):
-        ans = location
-    
-print(ans)
-    
+print(min(seeds)[0])
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-for sett in game[7:].split(';'):
-    #     for cube in sett.split(','):
-    #         freq = int(cube.strip().split()[0])
-    #         color = cube.strip().split()[1]
-    #         if (color == "red" and freq > 12) or (color == "blue" and freq > 14) or (color == "green" and freq > 13):
-    #             ok = False
+append
